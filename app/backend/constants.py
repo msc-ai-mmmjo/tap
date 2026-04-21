368125bd54b64b7454dea5fb84028574b6149846
Justin Mak <9521840+case547@users.noreply.github.com>
2026-04-21 16:33:10 +0100

    Update env

--- app/backend/constants.py
+++ app/backend/constants.py
@@ -7,5 +7,9 @@ load_dotenv(override=True)
 HF_TOKEN = os.getenv("HF_TOKEN")
 HF_CACHE_DIR = os.getenv("HF_CACHE_DIR")
 
-BERT_MCQ_DETECTION = os.getenv("BERT_MCQ_DETECTION")  # use BERT or Hydra
-MCQ_PROB_THRESHOLD = 0.5  # for classifying question as MCQ based on first token logits
+BERT_MCQ_DETECTION = os.getenv("BERT_MCQ_DETECTION", "False").lower() in (
+    "true",
+    "1",
+    "t",
+)
+MCQ_PROB_THRESHOLD = float(os.getenv("MCQ_PROB_THRESHOLD", "0.5"))
